556da82fdbc5d0ddfeffde8a7c97352ae4cd4af1
Kingsley <divine10646@gmail.com>
2022-08-06 16:29:06 +0100

    added user authentication

--- index.js
+++ index.js
@@ -1,18 +1,79 @@
 const express = require('express')
 const dotenv = require('dotenv')
+const cors = require('cors')
+const bodyParser = require('body-parser')
 const http = require('http')
 const authRoutes = require('./routes/auth.routes')
 const path = require('path')
+const nodemailer = require('nodemailer')
+const smtpTransport = require('nodemailer-smtp-transport')
 
 dotenv.config()
 const PORT = process.env.PORT || 4001
 const app = express()
 
+app.use(cors({
+    origin: '*'
+}))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+
+const smtpConfig = smtpTransport({
+    host: 'mail.vendorcrest.com',
+    secure: false,
+    tls: {
+        rejectUnauthorized: false
+    },
+    port: 587,
+    auth: {
+        user: '_mainaccount@vendorcrest.com',
+        pass: '@0N0q1vyDwAW2+'
+    }
+})
+
+const transporter = nodemailer.createTransport(smtpConfig)
+
+let mailOptions = {
+    from: 'Vendorcrest Digital <info@vendorcrest.com>',
+    sender: 'divine10646@gmail.com',
+    to: 'kingsleyakahibe@gmail.com',
+    replyTo: 'divine10646@gmail.com',
+    subject: 'Here is the subject',
+    html: '<h2>This is a another test email message.<h2/>',
+}
+
 app.use(express.static(path.join(__dirname, 'client', 'build')))
+
+app.use((req, res, next) => {
+    console.log(req?.body ?? {})
+
+    if (req.method === 'POST') {
+        // check if request body is empty
+        if (!req.body) {
+            res.status(422).json({
+                status: 'error', 
+                message: 'unprocessable entity'
+            })
+            return ;
+        }
+    }
+
+    next()
+})
 
 app.use('/auth', authRoutes)
 
-
+app.get('/sendmail', async function(req, res) {
+    console.log('sendmail request processing...')
+    try {
+        const info = await transporter.sendMail(mailOptions)
+        console.log('Info: ', info)
+        res.send('Email sent!')
+    } catch (error) {
+        console.log('Error', error)
+        res.send('An error occurred!')
+    }
+})
 
 
 app.listen(PORT, () => {
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,19 +1,93 @@
 const express = require('express')
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
+const { PrismaClient } = require('@prisma/client')
 
 const router = express.Router()
+const prisma = new PrismaClient()
 
 router.get('/signup', (req, res) => {
     console.log('signup route called...')
     res.send('Start signup process')
 })
 
-router.post('/signup', (req, res) => {
-    console.log(req.body)
+router.post('/signup', async (req, res) => {
+
+    // validate request credentials
+    const {name, email, password} = req.body ?? {}
+    if (!name ?? !email ?? !password) {
+        res.status(400).send({status: 'error', message: 'fill in all fields'})
+        return;
+    }
+
+    // check if user exixts
+    let userExists = await prisma.users.findUnique({
+        where: { email: email}
+    })
+    if (userExists) {
+        console.log(userExists)
+        res.status(400).send({status: 'error', message: 'user already exists'})
+        return;
+    }
+
+    // hash password and save user info
+    let user;
+    try {
+        let salt = await bcrypt.genSalt(10)
+        let passwordHash = await bcrypt.hash(password, salt)
+        user = await prisma.users.create({
+            data: {
+                name, email, passwordHash
+            }
+        })
+        console.log(user)
+        res.status(200).send({
+            status: 'success', 
+            message: 'user registered sucessfully'
+        })
+    } catch (error) {
+        console.log(error)
+        res.status(500).send({status: 'error', message: 'internal server error'})
+    }
     
 })
 
-router.post('/login', (req, res) => {
-    console.log(req.body)
+router.post('/login', async (req, res) => {
+    const { email, password } = req.body ?? {}
+
+    // validate user credentials
+    if (!email || !password) {
+        res.status(400).send({status: 'error', message: 'fill in all fields'})
+        return;
+    }
+
+    let user;
+    try {
+        user = await prisma.users.findUnique({
+            where: {email: email}
+        })
+        if (!user) {
+            res.status(401).send({status: 'error', message: 'user does not exist'})
+            return;
+        }
+        let validPassword = await bcrypt.compare(password, user.passwordHash)
+        if (!validPassword) {
+            res.status(400).send({status: 'error', message: 'invalid password'})
+            return;
+        }
+    } catch (error) {
+        console.log(error)
+        res.status(500).send({status: 'error', message: 'internal server error'})
+    }
+
+    let token;
+    try {
+        token = jwt.sign({id: user.id}, 'very-secret-key', {expiresIn: "1h"})
+        res.status(200).send({status: 'success', data: {user, token}})
+    } catch (error) {
+        console.log(error)
+        res.status(500).send({status: 'error', message: 'internal server error'})
+    }
 })
 
 
